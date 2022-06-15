af41305b27bcd5458b2d48d25139f7479f58ab93
TagaevRenat <renattagaev0086@gmail.com>
2022-06-15 12:54:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,69 +1,71 @@
-let target = document.querySelector('.countainer')
-target.addEventListener('mousedown', (event) => fill(event))
+window.onload = () => {
+    let target = document.querySelector('.countainer')
+    target.addEventListener('mousedown', (event) => fill(event))
 
-let turnValue = document.querySelector('.turn')
-let counter = 0
+    let turnValue = document.querySelector('.turn')
+    let counter = 0
 
-function fill(event) {
-    if (event.target.textContent == '') {
-        let sign = (counter % 2 == 0) ? event.target.innerHTML = 'X' : event.target.innerHTML = '0'
-        counter++
+    function fill(event) {
+        if (event.target.textContent == '') {
+            let sign = (counter % 2 == 0) ? event.target.innerHTML = 'X' : event.target.innerHTML = '0'
+            counter++
+        }
+        if (event.target.textContent == 'RESET GAME') {
+            document.location.reload()
+        }
+        let turn = (counter % 2 == 0) ? turnValue.innerHTML = "X's TURN" : turnValue.innerHTML = "O's TURN"
+        memory(event.target.id, turn.slice(0, 1))
     }
-    if (event.target.textContent == 'RESET GAME') {
-        document.location.reload()
-    }
-    let turn = (counter % 2 == 0) ? turnValue.innerHTML = "X's TURN" : turnValue.innerHTML = "O's TURN"
-    memory(event.target.id, turn.slice(0, 1))
-}
 
-let winCells = [
-    ['1', '2', '3'],
-    ['7', '8', '9'],
-    ['1', '4', '7'],
-    ['2', '5', '8'],
-    ['3', '6', '9'],
-    ['1', '5', '9'],
-    ['4', '5', '6'],
-    ['3', '5', '7']
-]
+    let winCells = [
+        ['1', '2', '3'],
+        ['7', '8', '9'],
+        ['1', '4', '7'],
+        ['2', '5', '8'],
+        ['3', '6', '9'],
+        ['1', '5', '9'],
+        ['4', '5', '6'],
+        ['3', '5', '7']
+    ]
 
-let x = []
-let o = []
+    let x = []
+    let o = []
 
 
-function memory(id, sign) {
-    if (id == '') {
-        return
-    }
-    if (sign == 'O') {
-        x.push(id)
-        checkWinner(x, 'X')
-    }
-    else {
-        o.push(id)
-        checkWinner(o, 'O')
-    }
-}
-
-let winner = []
-
-function checkWinner(signResult, player) {
-    for (let i = 0; i < winCells.length; i++) {
-        winner = []
-        for (let j = 0; j < signResult.length; j++) {
-            if (winCells[i].includes(signResult[j])) {
-                winner.push(player)
-            }
-            if (winner.length == 3) {
-                alert(`Winner is ${player}`)
-                checkWinner = NaN
-                break
-            }
+    function memory(id, sign) {
+        if (id == '') {
+            return
+        }
+        if (sign == 'O') {
+            x.push(id)
+            checkWinner(x, 'X')
+        }
+        else {
+            o.push(id)
+            checkWinner(o, 'O')
         }
     }
-    if ((x.length + o.length) == 9) {
-        alert('No winners, no losers, worthy fight!')
-        checkWinner = NaN
-        return
+
+    let winner = []
+
+    function checkWinner(signResult, player) {
+        for (let i = 0; i < winCells.length; i++) {
+            winner = []
+            for (let j = 0; j < signResult.length; j++) {
+                if (winCells[i].includes(signResult[j])) {
+                    winner.push(player)
+                }
+                if (winner.length == 3) {
+                    alert(`Winner is ${player}`)
+                    checkWinner = NaN
+                    break
+                }
+            }
+        }
+        if ((x.length + o.length) == 9) {
+            alert('No winners, no losers, worthy fight!')
+            checkWinner = NaN
+            return
+        }
     }
 }
